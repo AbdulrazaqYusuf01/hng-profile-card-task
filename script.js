32a5d9a4e40a98e10f6d4f890ccc7cb009de4570
Abdulrazaq <abdulrazaqmyusuf@gmail.com>
2025-10-18 11:30:37 +0100

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// script.js
 function updateTime() {
-  const timeElement = document.getElementById('current-time');
+  const timeElement = document.getElementById("current-time");
   timeElement.textContent = Date.now();
 }
 updateTime();
-setInterval(updateTime, 1000); // optional, updates every second
+setInterval(updateTime, 1000);
